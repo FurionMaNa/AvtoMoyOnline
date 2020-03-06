0c20d71c75d5f41d6497f1a3f5ec6d7fd3d10f0a
MaNa <andreygorlov19995@gmail.com>
2020-03-06 15:54:39 +0300

    Commit 3

--- app/src/main/java/com/project/avtomoy/WorkTimeClass.java
+++ app/src/main/java/com/project/avtomoy/WorkTimeClass.java
@@ -9,69 +9,69 @@ public class WorkTimeClass {
     private String Пятница;
     private String Суббота;
 
-    public WorkTimeClass(String воскресенье, String понедельник, String вторник, String среда, String четверг, String пятница, String суббота) {
-        Воскресенье = воскресенье;
-        Понедельник = понедельник;
-        Вторник = вторник;
-        Среда = среда;
-        Четверг = четверг;
-        Пятница = пятница;
-        Суббота = суббота;
+    public WorkTimeClass(String sunday, String monday, String tuesday, String wednesday, String thursday, String friday, String saturday) {
+        Воскресенье = sunday;
+        Понедельник = monday;
+        Вторник = tuesday;
+        Среда = wednesday;
+        Четверг = thursday;
+        Пятница = friday;
+        Суббота = saturday;
     }
 
     public String getВоскресенье() {
         return Воскресенье;
     }
 
-    public void setВоскресенье(String воскресенье) {
-        Воскресенье = воскресенье;
+    public void setВоскресенье(String sunday) {
+        Воскресенье = sunday;
     }
 
     public String getПонедельник() {
         return Понедельник;
     }
 
-    public void setПонедельник(String понедельник) {
-        Понедельник = понедельник;
+    public void setПонедельник(String monday) {
+        Понедельник = monday;
     }
 
     public String getВторник() {
         return Вторник;
     }
 
-    public void setВторник(String вторник) {
-        Вторник = вторник;
+    public void setВторник(String tuesday) {
+        Вторник = tuesday;
     }
 
     public String getСреда() {
         return Среда;
     }
 
-    public void setСреда(String среда) {
-        Среда = среда;
+    public void setСреда(String wednesday) {
+        Среда = wednesday;
     }
 
     public String getЧетверг() {
         return Четверг;
     }
 
-    public void setЧетверг(String четверг) {
-        Четверг = четверг;
+    public void setЧетверг(String thursday) {
+        Четверг = thursday;
     }
 
     public String getПятница() {
         return Пятница;
     }
 
-    public void setПятница(String пятница) {
-        Пятница = пятница;
+    public void setПятница(String friday) {
+        Пятница = friday;
     }
 
     public String getСуббота() {
         return Суббота;
     }
 
-    public void setСуббота(String суббота) {
-        Суббота = суббота;
+    public void setСуббота(String saturday) {
+        Суббота = saturday;
     }
 }
